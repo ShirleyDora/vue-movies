f74fc006e54ab340f28740c243e1be2705367a14
Shirley <565914036@qq.com>
2020-07-01 04:09:05 +0800

    master

--- src/views/photo/PhotoDetail.vue
+++ src/views/photo/PhotoDetail.vue
@@ -2,20 +2,51 @@
     <!-- <h1>{{$route.params.src}}</h1> -->
     <!-- <h1>{{$route.params.idx}}</h1> -->
     <!-- <h1>{{imgSrc}}</h1> -->
-    <div @tap="callback" @click="$router.go(-1)" class="img" :style="{background:'url('+imgSrc+') no-repeat center/contain #000'}">
-        
-    </div>
+    <router-link to="/photo">
+        <v-touch @swipeleft="swipeleft" @swiperight="swiperight" class="img" :style="{background:'url('+imgSrc+') no-repeat center/contain #000'}">
+        </v-touch>
+    </router-link>
+   
 </template>
 
 <script>
     export default {
         data(){
             return{
-                imgSrc:''
+                imgSrc:'',
+                nowIndex:-1
+            }
+        },
+        watch:{
+            nowIndex(){
+                let nowImg = this.$store.state.photoList[this.nowIndex];
+                if(nowImg){
+                    this.imgSrc = nowImg.src;
+                }
             }
         },
         created(){
-            this.imgSrc = this.$store.state.photoList[this.$route.params.idx].src;
+            this.nowIndex = this.$route.params.idx;
+            // let nowImg = this.$store.state.photoList[this.nowIndex];
+            // if(nowImg){
+            //     this.imgSrc = this.$store.state.photoList[this.nowIndex].src;
+            // }
+        },
+        methods:{
+            swiperight(){
+                // console.log('向右')
+                this.nowIndex--;
+                if(this.nowIndex == -1){
+                    this.nowIndex = this.$store.state.photoList.length;
+                }
+            },
+            swipeleft(){
+                // console.log('向左')
+                this.nowIndex++;
+                if(this.nowIndex == this.$store.state.photoList.length){
+                    this.nowIndex = 0;
+                }
+            }
         }
     }
 </script>
@@ -23,8 +54,8 @@
 <style lang="scss" scoped>
 .img{
     position:absolute;
-    top:0;
-    bottom:0;
+    top:1rem;
+    bottom:1rem;
     left:0;
     right:0;
 }
